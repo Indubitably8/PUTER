8f242fb0eef331c8072d335d46cf3ac02130421b
John <113923030+Indubitably8@users.noreply.github.com>
2026-01-24 21:33:50 -0500

    Appshell fix
    - Removed plugin in favor of native dart stuff

--- puter_interface/lib/app_shell.dart
+++ puter_interface/lib/app_shell.dart
@@ -1,42 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:ui' show AppExitResponse;
+import 'package:flutter/widgets.dart';
 import 'package:puter_interface/system/command_runner.dart';
-import 'package:window_manager/window_manager.dart';
 
 class AppShell extends StatefulWidget {
   final Widget child;
-  const AppShell({super.key, required this.child});
+
+  const AppShell({
+    super.key,
+    required this.child,
+  });
 
   @override
-  State<AppShell> createState() => _AppShellState();
+  State<AppShell> createState() => _ExitCleanupState();
 }
 
-class _AppShellState extends State<AppShell> with WindowListener {
-  bool _closing = false;
+class _ExitCleanupState extends State<AppShell> with WidgetsBindingObserver {
+  bool _ran = false;
 
   @override
   void initState() {
     super.initState();
-    windowManager.addListener(this);
-    windowManager.setPreventClose(true);
+    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
   void dispose() {
-    windowManager.removeListener(this);
+    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
   @override
-  void onWindowClose() async {
-    if (_closing) return;
-    _closing = true;
-
-    try {
-      CommandRunner.bash("pkill", ["spotifyd"]);
-    } catch (_) {}
-
-    await windowManager.setPreventClose(false);
-    await windowManager.close();
+  Future<AppExitResponse> didRequestAppExit() async {
+    if (!_ran) {
+      _ran = true;
+      try {
+        await CommandRunner.bash("pkill", ["spotifyd"]);
+      } catch (_) {
+        // don't trap the user in the app because cleanup failed
+      }
+    }
+    return AppExitResponse.exit;
   }
 
   @override
